84852c9eb69ca256326cb15caa304cc610adf882
madlab2 <pnsm@arcor.de>
2024-01-15 13:48:38 +0100

    added config for predict, started main for predictor

--- breast_cancer_segmentation/predicter/main.py
+++ breast_cancer_segmentation/predicter/main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from hydra import compose, initialize
+import logging
+import torch
+
+
+log = logging.getLogger(__name__)
 
 app = FastAPI()
+with initialize(version_bas=None, config_path="../../config/hydra"):
+    config = compose(config_name="config_hydra.yaml")
+    # path to scripted neural net (with weights)
+    model_path = config.train_hyp.model_repo_location + config.predict_hyp.model_filename
+    unet_model = torch.jit.load(model_path)
 
-class PredictionInputModel(BaseModel):
-    value: str
 
 @app.get("/health")
 def read_health():
     return 200
 
+
 @app.post("/predict")
-def read_item(input: PredictionInputModel):
-    return {"value": f'{input.value} world!'}
-
-
+def read_item(input):
+    # todo: we want to have top classes, not probs
+    # todo: we want to get back original image as well
+    return unet_model(input)
