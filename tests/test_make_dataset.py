3fc53298bf6bd9745a0e447253aa0f0dd7bfbbc1
Thomas2710 <thomas.trevisan@studenti.unitn.it>
2024-01-15 10:50:14 +0100

    Printing path

--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -38,6 +38,7 @@ def create_transforms():
 def test_train_data():
     with initialize(version_base=None, config_path="./../config/hydra/"):
         config = compose(config_name="config_hydra.yaml")
+        print(f"current dir files are {os.listdir()}")
         print(os.getcwd())
         print(config)
         path = config.train_hyp["train_img_location"]
