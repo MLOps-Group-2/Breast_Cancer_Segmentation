400c0b63de3548fe8c5437e535c11f96b7e54dd8
villi <villi@runicdices.com>
2024-01-16 10:55:43 +0100

    load model experiments

--- breast_cancer_segmentation/predicter/main.py
+++ breast_cancer_segmentation/predicter/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, UploadFile, File
 from http import HTTPStatus
 from hydra import compose, initialize
+import io
 import logging
 import torch
 import torchvision.transforms as transforms
+
+from PIL import Image
 from breast_cancer_segmentation.models.UNETModel import UNETModel  # noqa
 
 log = logging.getLogger(__name__)
@@ -14,7 +17,7 @@ with initialize(version_base=None, config_path="../../config/hydra"):
     # path to scripted neural net (with weights)
     # model_path = config.train_hyp.model_repo_location + config.predict_hyp.model_filename
     # unet_model = None
-    model_path = "./models/model-local.pt"
+    model_path = "./models/model.pt"
     unet_model = torch.jit.load(model_path)
 
 
@@ -25,31 +28,32 @@ def read_health():
 
 @app.post("/predict-img/")
 async def predict_img(data: UploadFile = File(...)):
-    with open("image.jpg", "wb") as image:
-        # content = await data.read()
-        # image.write(content)
-        pil_image = data.file.read()
-        transform = transforms.Compose(
-            [
-                transforms.Resize((224, 224)),  # Adjust size according to your model's input size
-                transforms.ToTensor(),
-            ]
-        )
-        image_tensor = transform(pil_image)
-        image_tensor = image_tensor.unsqueeze(0)
+    content = data.file.read()
+    pil_image = Image.open(io.BytesIO(content))
 
-        scores = unet_model(input)  # [1, 3, dim, dim]
-        values, indices = torch.topk(scores, k=1, dim=1)
-        print(indices)
+    transform = transforms.Compose(
+        [
+            transforms.Resize((224, 224)),  # Adjust size according to your model's input size
+            transforms.ToTensor(),
+        ]
+    )
 
-        # somehow create a tensor with the image data
-        # image_tensor = torch.frombuffer(image, dtype=float)
-        # plt.show(image_tensor)
-        image.close()
+    image_tensor = transform(pil_image)
+    image_tensor = image_tensor.unsqueeze(0)
+
+    #scores = unet_model(image_tensor)  # [1, 3, dim, dim]
+    #values, indices = torch.topk(scores, k=1, dim=1)
+    #print(indices)
+
+    # somehow create a tensor with the image data
+    # image_tensor = torch.frombuffer(image, dtype=float)
+    # plt.show(image_tensor)
+
     response = {
         "input": data,
+        "input_shape": str(image_tensor.shape),
         "message": HTTPStatus.OK.phrase,
-        "status-code": HTTPStatus.OK,
+        "status-code": HTTPStatus.OK
     }
     return response
 
